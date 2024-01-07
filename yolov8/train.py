1987545a86435b734fdd7197d42ef2668a4de034
abdulmalik-99 <85821232+abdulmalik-99@users.noreply.github.com>
2024-01-07 23:42:17 +0300

    Update train.py

--- yolov8/train.py
+++ yolov8/train.py
@@ -1,12 +1,22 @@
+import argparse
 from ultralytics import YOLO
 
-data_path=''
-model_path=''
-number_of_epoch=
-# Load a model
-model = YOLO("yolov8x.pt")  # load a pretrained model (recommended for training)
+def train_and_validate(data_path, model_path, number_of_epochs):
+    # Load a model
+    model = YOLO(model_path)  # load a pretrained model (recommended for training)
 
-# Use the model
-model.train(data=data_path, epochs=number_of_epoch)
+    # Use the model for training
+    model.train(data=data_path, epochs=number_of_epochs)
 
-model.val(data=data_path)
+    # Validate the model
+    model.val(data=data_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train and validate YOLO model.")
+    parser.add_argument('--data_path', type=str, help='Path to the training data', required=True)
+    parser.add_argument('--model_path', type=str, help='Path to the pretrained model', required=True)
+    parser.add_argument('--number_of_epochs', type=int, help='Number of training epochs', required=True)
+
+    args = parser.parse_args()
+
+    train_and_validate(args.data_path, args.model_path, args.number_of_epochs)
